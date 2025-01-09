08152ca612419648b886914d2fac704fe0063ade
Alexander Arrazate <isaizander09@gmail.com>
2025-01-08 23:54:09 -0600

    Se agrego un nuevo proyecto al portafolio y se le agregaron el uso de los principios SOLID al primer proyecto

--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -8,7 +8,7 @@ export const Portfolio = () => {
 
   const keywords = [
     "TypeScript", "JavaScript", "Tailwind CSS", "React", "Node.js", "Clothes", "Shop", "App", "URLS", "responsive","tests", "Jest",
-    "Socket.io", "MongoDB", "Google API", "Giphy API", "geolocation API", "API", "REST", "Weather", "Cloudinary"
+    "Socket.io", "MongoDB", "Google API", "Giphy API", "geolocation API", "API", "REST", "Weather", "Cloudinary", "SOLID", "chart.js"
   ];
 
   const highlightKeywords = (text) => {
@@ -29,47 +29,54 @@ export const Portfolio = () => {
       numberOfProject: "P R O J E C T  1",
       title: "Clothes Shop App",
       description:
-        "Built with TypeScript, this application uses React for the Front End and Node.js for the Back End. The design is fully responsive, implemented using Tailwind CSS. The main functionality involves retrieving products from a MongoDB database. The images associated with the products are stored in the cloud via Cloudinary, allowing the database to only store the corresponding URLs, organized by clothing categories. This approach optimizes the application's performance by reducing server load and improving content delivery efficiency.",
+        "Built with TypeScript and designed with an architecture based on SOLID principles, this application uses React for the Front End and Node.js for the Back End. The design is fully responsive, implemented using Tailwind CSS. The main functionality involves retrieving products from a MongoDB database. The images associated with the products are stored in the cloud via Cloudinary, allowing the database to only store the corresponding URLs, organized by clothing categories. This approach optimizes the application's performance by reducing server load and improving content delivery efficiency.",
       tech: "Front End And Back End",
       img: '/assets/portfolio/CLOTHES_SHOP.png',
     },
     {
-      numberOfProject: "P R O J E C T 2",
+      numberOfProject: "P R O J E C T  2",
+      title: "Dashboard",
+      description:"Developed using TypeScript, with an architecture based on SOLID principles. The frontend is implemented in React, while the backend is built with Node.js, using MongoDB as the database for data storage. For data visualization, the Chart.js library was utilized, enabling the creation of interactive charts. The application's primary functionality is to display monthly metrics for an online store, including sales, stock outflows, inflows, and the most popular products.",
+      tech: "Front End And Back End",
+      img: '/assets/portfolio/DASHBOARD.png',
+    },
+    {
+      numberOfProject: "P R O J E C T 3",
       title: "Calendar App",
       description: "Developed in JavaScript, the application uses React for the Front End  and Node.js for the Back End. Its main functionality focuses on creating events within a calendar, where each event can include start and end dates, as well as detailed descriptions. To create, edit, or delete an event, the user must be authenticated, and only the event creator has the permissions to modify or remove it. While users can view events created by others, they cannot edit or delete them.",
       tech: "Front End And Back End",
       img: '/assets/portfolio/CALENDAR_APP.png'
     },
     {
-      numberOfProject: "P R O J E C T 3",
+      numberOfProject: "P R O J E C T 4",
       title: "Socket App",
       description: "Developed in JavaScript, the application uses React for the Front End , Node.js for the Back End, and Socket.io to manage real-time communication between users. The main functionality is based on a ticket management system, where each counter can generate its own tickets. Additionally, in a separate view, users can check the status of tickets in the queue, as well as the ticket currently being handled.",
       tech: "Front End And Back End",
       img: '/assets/portfolio/SOCKET_COLAS.png'
     },
     {
-      numberOfProject: "P R O J E C T 4",
+      numberOfProject: "P R O J E C T 5",
       title: "Google Sign-In",
       description: 'Developed using JavaScript and React, the application’s main functionality is the integration with the Google API, allowing users to authenticate via their Google account. This implementation is based on a REST API that was later used in the Clothes Shop App project, but coded in TypeScript, which explains the increased complexity seen in the code editor displayed in the image.',
       tech: "Back End",
       img: '/assets/portfolio/GOOGLE SIGN-IN.png'
     },
     {
-      numberOfProject: "P R O J E C T 5",
+      numberOfProject: "P R O J E C T 6",
       title: "Gif App",
       description: "Developed with JavaScript and React, the main functionality of the application is the integration of the Giphy API. Through a search box, users can search for and explore GIFs related to various topics. Additionally, I implemented unit tests using Jest to ensure the reliability and correctness of the application's features.",
       tech: "Front End",
       img: '/assets/portfolio/GIF APP.png'
     },
     {
-      numberOfProject: "P R O J E C T 6",
+      numberOfProject: "P R O J E C T 7",
       title: "Climate App",
       description: "Developed using JavaScript and Node.js, the application interacts with a Weather Geolocation API. Through an intuitive terminal interface, users can query various cities around the world and receive updated weather information along with additional relevant data.",
       tech: "Back End",
       img: '/assets/portfolio/CLIMATE APP.png'
     },
     {
-      numberOfProject: "P R O J E C T 7",
+      numberOfProject: "P R O J E C T 8",
       title: "Heroes App",
       description: "Developed in JavaScript with React, this application consists solely of the Front End, due to its simple and straightforward design. Its main functionality is to display heroes from their respective universes, Marvel Comics or DC Comics. Additionally, it offers a dedicated view for performing specific hero searches.",
       tech: "Front End",
